e595b9e451e02625cf699afc8581e45f7580de7f
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-07-05 09:33:19 +0000

    Added base.html template file

--- feedpost/views.py
+++ feedpost/views.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('created_at')
-    template_name = 'profile.html'
+    template_name = 'base.html'
