eb976f424a20c2df5f3b1877f315a165df7e192b
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-08-03 14:50:52 +0000

    Added an AddPost class in the view and created a path in url

--- feedpost/urls.py
+++ feedpost/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('add_post/', views.AddPostView.as_view(), name='add_post'),
 ]
--- feedpost/views.py
+++ feedpost/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.http import JsonResponse
-from django.views import generic
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
-class PostList(generic.ListView):
+class PostList(ListView):
     model = Post
     queryset = Post.objects.order_by('created_at')
     template_name = 'profile.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
